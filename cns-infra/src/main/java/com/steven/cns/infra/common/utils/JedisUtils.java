26869478b660b41f7d353c9e66e1419a75568711
曹磊 <caolei19@tal.com>
2023-05-15 10:57:14 +0800

    feat(JedisUtils文件修改): JedisUtils文件修改
    JedisUtils文件修改

--- cns-infra/src/main/java/com/steven/cns/infra/common/utils/JedisUtils.java
+++ cns-infra/src/main/java/com/steven/cns/infra/common/utils/JedisUtils.java
@@ -1,45 +1,148 @@
 package com.steven.cns.infra.common.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.params.SetParams;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * jedis 工具类
  *
  * @author steven.cao
  */
-public final class JedisUtils {
+@ConditionalOnProperty(name = "spring.redis.jedis.pool.enable", havingValue = "true", matchIfMissing = true)
+@Component
+public class JedisUtils {
 
-    private static JedisPool jedisPool;
+//    @Autowired
+    private JedisPool jedisPool;
 
-    public static void init(JedisPool jedisPool) {
-        JedisUtils.jedisPool = jedisPool;
-    }
-
-    public static Jedis getResource() {
+    public Jedis getResource() {
         return jedisPool.getResource();
     }
 
-    public static void returnResource(Jedis jedis) {
-        jedis.close();
-    }
-
-    public static void set(String key, String value) {
-        try (Jedis jedis = getResource()) {
-            jedis.set(key, value);
+    public void returnResource(Jedis jedis) {
+        if (jedis != null) {
+            jedis.close();
         }
     }
 
-    public static String get(String key) {
-        try (Jedis jedis = getResource()) {
+    public String get(String key) {
+        Jedis jedis = null;
+        try {
+            jedis = getResource();
             return jedis.get(key);
+        } finally {
+            returnResource(jedis);
         }
     }
 
-    public static void del(String key) {
-        try (Jedis jedis = getResource()) {
+    public void set(String key, String value) {
+        Jedis jedis = null;
+        try {
+            jedis = getResource();
+            jedis.set(key, value);
+        } finally {
+            returnResource(jedis);
+        }
+    }
+
+    public void setex(String key, String value, int seconds) {
+        Jedis jedis = null;
+        try {
+            jedis = getResource();
+            jedis.setex(key, seconds, value);
+        } finally {
+            returnResource(jedis);
+        }
+    }
+
+    public void del(String key) {
+        Jedis jedis = null;
+        try {
+            jedis = getResource();
             jedis.del(key);
+        } finally {
+            returnResource(jedis);
         }
     }
 
+    public void incr(String key) {
+        Jedis jedis = null;
+        try {
+            jedis = getResource();
+            jedis.incr(key);
+        } finally {
+            returnResource(jedis);
+        }
+    }
+
+    public void decr(String key) {
+        Jedis jedis = null;
+        try {
+            jedis = getResource();
+            jedis.decr(key);
+        } finally {
+            returnResource(jedis);
+        }
+    }
+
+    public void expire(String key, int seconds) {
+        Jedis jedis = null;
+        try {
+            jedis = getResource();
+            jedis.expire(key, seconds);
+        } finally {
+            returnResource(jedis);
+        }
+    }
+
+    public void setList(String key, List<String> list) {
+        Jedis jedis = null;
+        try {
+            jedis = getResource();
+            for (String value : list) {
+                jedis.rpush(key, value);
+            }
+        } finally {
+            returnResource(jedis);
+        }
+    }
+
+    public List<String> getList(String key) {
+        Jedis jedis = null;
+        try {
+            jedis = getResource();
+            return jedis.lrange(key, 0, -1);
+        } finally {
+            returnResource(jedis);
+        }
+    }
+
+    public void setHash(String key, Map<String, String> map) {
+        Jedis jedis = null;
+        try {
+            jedis = getResource();
+            jedis.hmset(key, map);
+        } finally {
+            returnResource(jedis);
+        }
+    }
+
+    public Map<String, String> getHash(String key) {
+        Jedis jedis = null;
+        try {
+            jedis = getResource();
+            return jedis.hgetAll(key);
+        } finally {
+            returnResource(jedis);
+        }
+    }
+
+    // add other methods ...
 }
